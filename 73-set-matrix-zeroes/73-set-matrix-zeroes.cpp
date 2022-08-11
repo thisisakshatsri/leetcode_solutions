e7826d0bccc223e8cf52189e74638b13d2a878be
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-12 01:11:31 +0530

    Time: 11 ms (96.97%), Space: 13.3 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -6,8 +6,8 @@ public:
         
         bool row = false, col = false;
         
-        for(int i=0; i<n; i++) if(matrix[i][0] == 0) col = true;
-        for(int j=0; j<m; j++) if(matrix[0][j] == 0) row = true;
+        for(int i=0; i<n; i++) if (matrix[i][0] == 0) col = true;
+        for(int j=0; j<m; j++) if (matrix[0][j] == 0) row = true;
         
         for(int i=1; i<n; i++){
             for(int j=1; j<m; j++){
@@ -31,9 +31,8 @@ public:
         }
         
         if(col){
-            for(int i=0; i<n; i++) matrix[i][0] =0;
+            for(int i=0; i<n; i++) matrix[i][0] = 0;
         }
-        
         if(row){
             for(int j=0; j<m; j++) matrix[0][j] = 0;
         }
