4948f2429091a38f193d06502838c5c320766161
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-16 21:23:06 +0530

    Time: 3 ms (76.26%), Space: 9.1 MB (30.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        int ans[n+m];
-        int i=0, j=0, k=0;
+        int i=0, j=0;
+        int arr[n+m];
+        int k=0;
         
         while(i < m and j < n) {
-            if(nums1[i] <= nums2[j]){
-                ans[k++] = nums1[i++];
+            if(nums1[i] <= nums2[j]) {
+                arr[k++] = nums1[i++];
             }
-            else{
-                ans[k++] = nums2[j++];
-            }
+            else arr[k++] = nums2[j++];
         }
         while(i < m) {
-            ans[k++] = nums1[i++];
+            arr[k++] = nums1[i++];
         }
-        while(j < n) {
-            ans[k++] = nums2[j++];
+        while(j  < n){
+            arr[k++] = nums2[j++];
         }
-        for(int x: ans) cout<<x<<" ";
         k=0;
-        for(int l=0; l<n+m; l++)
-            nums1[l] = ans[k++];
-        
+        for(int i=0; i<(n+m); i++) {
+            nums1[i] = arr[k++];
+        }
     }
 };
