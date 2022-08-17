e5cbe6de2e6bd2787e4e4bdfdef6f46fba980d78
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-18 03:24:22 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (71.01%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -5,25 +5,17 @@ public:
         stack<int> s;
         int n = nums2.size();
         unordered_map<int, int> mp;
-        
-        
+
         for(int i = n-1; i>=0; i--) {
-            if(s.size() == 0) v.push_back(-1);
-            else if(s.size() > 0 && s.top() > nums2[i]) v.push_back(s.top());
-            else if(s.size() > 0 && s.top() <= nums2[i]){
-                while(s.size() > 0 && s.top() <= nums2[i]) {
-                    s.pop();
-                }
-                if(s.size() == 0) v.push_back(-1);
-                else v.push_back(s.top());
+            while(!s.empty() && s.top() <= nums2[i] ) {
+                s.pop();
             }
+            int res = (s.empty()) ? -1 : s.top();
+            
+            mp.insert({nums2[i], res});
             s.push(nums2[i]);
         }
         
-        reverse(v.begin(), v.end());
-        for(int i=0; i<v.size(); i++) {
-            mp[nums2[i]] = v[i];
-        }
         vector<int> res;
         for(auto x: nums1) {
             res.push_back(mp[x]);
