e4c51263058bf8258b913f166cba089a6564b320
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-18 23:15:55 +0530

    Time: 199 ms (33.28%), Space: 20 MB (71.54%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
+        sort(nums.begin(), nums.end());
         
-        for(int i=0; i<nums.size(); i++) {
+        for(int i=0; i<n; i++) {
             int a = nums[i];
             int t = -a;
             int s = i+1;
             int e = n-1;
             
-            while(s < e){
+            while(s < e) {
                 if(nums[s] + nums[e] == t){
-                while(s<e && nums[s+1] == nums[s]) s++;
-                while(s<e && nums[e-1] == nums[e]) e--;
+                    while(s<e && nums[s] == nums[s+1]) s++;
+                    while(s<e && nums[e] == nums[e-1]) e--;
                     res.push_back({nums[i], nums[s], nums[e]});
                     s++;
                     e--;
                 }
-                else if(nums[s] + nums[e] > t) {
+                else if(nums[s] + nums[e] > t){
                     e--;
                 }
                 else{
                     s++;
                 }
             }
-            while(i+1<n &&nums[i+1] == nums[i]) i++;
+            while(i+1<n && nums[i] == nums[i+1]) i++;
         }
         return res;
     }
