0ac5b99e89f43599bc7feeeff98dccfcca7a7dfc
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-18 23:45:23 +0530

    Time: 370 ms (43.61%), Space: 78.3 MB (79.94%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        int n = arr.size();
+        vector<int> freq;
+        
+        for(int nums : arr){
+            mp[nums]++;
+        }
+        
+        for(auto x: mp){
+            freq.push_back(x.second);
+        }
+        sort(freq.begin(), freq.end());
+        reverse(freq.begin(), freq.end());
+        
+        int sum = 0;
+        int cnt = 0;
+        for(int i: freq){
+            sum += i;
+            cnt++;
+            if(sum >= n/2)
+                break;
+        }
+        return cnt;
+    }
+};
