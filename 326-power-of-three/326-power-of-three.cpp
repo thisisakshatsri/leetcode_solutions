be2a0a48f50719e16e9a1f3eb2f5074555ef47db
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-24 18:34:37 +0530

    Time: 31 ms (61.16%), Space: 5.8 MB (72.69%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -3,6 +3,8 @@ public:
     bool isPowerOfThree(int n) {
         if(n <= 0) return false;
         int k = pow(3, 19);
-        return (k % n == 0);
+        if(k % n == 0)
+            return true;
+        return false;
     }
 };
