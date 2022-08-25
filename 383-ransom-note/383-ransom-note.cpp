e8e31111d9391c2d9de2c8a9761d5bd1edbe2caf
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-25 10:12:08 +0530

    Time: 21 ms (65.89%), Space: 8.8 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int arr[26] = {0};
+        for(int i=0; i<magazine.length(); i++) {
+            arr[magazine[i]-'a']++;
+        }
+        for(int i=0; i<ransomNote.length(); i++) {
+            arr[ransomNote[i]-'a']--;
+        }
+        for(int i=0; i<26; i++) {
+            if(arr[i] < 0)
+                return false;
+        }
+        return true;
+    }
+};
