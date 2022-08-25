6b79a446bc1ee6278d30a578264bb614cdadb926
Akshat Srivastava <43510521+thisisakshatsri@users.noreply.github.com>
2022-08-25 10:45:23 +0530

    Time: 7 ms (55.57%), Space: 12.6 MB (63.20%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -24,7 +24,7 @@ public:
                 q.pop();
                 if(node->left != NULL) q.push(node->left);
                 if(node->right != NULL) q.push(node->right);
-                level.push_back(node->val);
+                level.emplace_back(node->val);
             }
             ans.push_back(level);
         }
